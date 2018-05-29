ad80bd95676286d2131e1b55a1103191b9567e79
Kinwave <hwt887@163.com>
2018-05-29 11:17:01 +0800

    3.4

--- XM-AUV-opencv/pi_usart.cpp
+++ XM-AUV-opencv/pi_usart.cpp
@@ -21,16 +21,16 @@ void usart_pthread_start(int boud)
 
 	if ((fd = serialOpen("/dev/ttyAMA0", boud)) < 0)
 	{
-        //printf("serial error\n");
+		//printf("serial error\n");
 	}
 	serialPuts(fd, "ready");
 	//serialPutchar(fd,serialGetchar(fd));
-    serialPutchar(fd, 0x12);
-	
-    //usart_send(0x0B);
+	serialPutchar(fd, 0x12);
+
+	//usart_send(0x0B);
 
 	pthread_t usart;
-    int ser = pthread_create(&usart, NULL, usart_read, NULL);
+	int ser = pthread_create(&usart, NULL, usart_read, NULL);
 	if (ser != 0)
 	{
 		std::cout << "pthread creat serial error" << std::endl;
@@ -39,35 +39,36 @@ void usart_pthread_start(int boud)
 
 void *usart_read(void *arg)
 {
-    while(1)
-    {
-        int a=0;
-        a=1;
-        if(dock_flag==1)
-        {
-            usart_send(0x0B);
-            usart_send(0X0C);
-            usart_send(0X53);
-            usart_send(0X4F);
-            dock_flag=0;
-        }
-        else if(dock_flag==2)
-        {
-            usart_send(0x0B);
-            usart_send(0X0C);
-            usart_send(0X4F);
-            usart_send(0X4F);
-            dock_flag=0;
-        }
-    }
+	while(1)
+	{
+		int a=0;
+		a=1;
+		if(dock_flag==1)
+		{
+			usart_send(0x0B);
+			usart_send(0X0C);
+			usart_send(0X53);
+			usart_send(0X4F);
+			dock_flag=0;
+		}
+		else if(dock_flag==2)
+		{
+			usart_send(0x0B);
+			usart_send(0X0C);
+			usart_send(0X4F);
+			usart_send(0X4F);
+			dock_flag=0;
+		}
+	}
 }
 
 
 
 void usart_send(int send)
 {
-    serialPutchar(fd, send);
+	serialPutchar(fd, send);
 }
 
 */
-//´òÁË¶à¾Ã°¡
+
+
